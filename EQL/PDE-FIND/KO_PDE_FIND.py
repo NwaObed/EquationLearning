bef95d031e4492b6a61d708f98d7b606c72ad9f2
NwaObed <Obed.Nwachukwu@warwick.ac.uk>
2022-09-12 09:45:17 +0100

    update

--- EQL/PDE-FIND/KO_PDE_FIND.py
+++ EQL/PDE-FIND/KO_PDE_FIND.py
@@ -3,6 +3,24 @@ from sklearn.linear_model import Ridge, RidgeCV
 import pde
 
 
+
+"""
+A few functions used in PDE-FIND
+Source: Samuel Rudy.  2016
+"""
+
+
+##################################################################################
+##################################################################################
+#
+# Functions for taking derivatives.
+# When in doubt / nice data ===> finite differences
+#               \ noisy data ===> polynomials
+#             
+##################################################################################
+##################################################################################
+
+
 def FiniteDiff(u, dx, d):
     """
     Takes dth derivative data using 2nd order finite difference method (up to d=3)
@@ -706,3 +724,92 @@ def optimize_tolerance(w_best,TrainR,TestR,TrainY,TestY):
             tol = tol + d_tol
 
     if print_best_tol: print("Optimal tolerance:", tol_best)
+        
+        
+def pdefind(X, y, epsilon = 0.1, maxit_f = 100, maxit_b = 5, backwards_freq = 5, relearn_f=True, relearn_b=True):
+    """
+    Forward-Backward greedy algorithm for sparse regression.
+    See Zhang, Tom. 'Adaptive Forward-Backward Greedy Algorithm for Sparse Learning with Linear Models', NIPS, 2008
+    The original version of this code that was uploaded github was contained errors.  This version has been corrected and
+    also includes an variation of FoBa used in Thaler et al. 'Sparse identification of truncation errors,' JCP, 2019,where 
+    we have additionally used relearning on the backwards step.  This later implementation (currently set as the default 
+    with relearn_f=relearn_b=True) relearns non-zero terms of w, rather than only fitting the residual, as was done in Zhang.  
+    It is slower, more robust, but still in some cases underperforms STRidge.
+    """
+
+    n,d = X.shape
+    F = {}
+    F[0] = set()
+    w = {}
+    w[0] = np.zeros((d,1))
+    k = 0
+    delta = {}
+
+    for forward_iter in range(maxit_f):
+
+        k = k+1
+
+        # forward step
+        zero_coeffs = np.where(w[k-1] == 0)[0]
+        if len(zero_coeffs)==0: return w[k-1]
+        
+        err_after_addition = []
+        residual = y - X.dot(w[k-1])
+        for i in zero_coeffs:
+
+            if relearn_f:
+                F_trial = F[k-1].union({i})
+                w_added = np.zeros((d,1))
+                w_added[list(F_trial)] = np.linalg.lstsq(X[:, list(F_trial)], y, rcond=None)[0]
+                
+            else:
+                # Per figure 3 line 8 in paper, do not retrain old variables.
+                # Only look for optimal alpha, which is solving for new w iff X is unitary
+                alpha = X[:,i].T.dot(residual)/np.linalg.norm(X[:,i])**2
+                w_added = np.copy(w[k-1])
+                w_added[i] = alpha
+
+            err_after_addition.append(np.linalg.norm(X.dot(w_added)-y))
+        i = zero_coeffs[np.argmin(err_after_addition)]
+        
+        F[k] = F[k-1].union({i})
+        w[k] = np.zeros((d,1), dtype=np.complex64)
+        w[k][list(F[k])] = np.linalg.lstsq(X[:, list(F[k])], y,rcond=None)[0]
+
+        # check for break condition
+        delta[k] = np.linalg.norm(X.dot(w[k-1]) - y) - np.linalg.norm(X.dot(w[k]) - y)
+        if delta[k] < epsilon: return w[k-1]
+
+        # backward step, do once every few forward steps
+        if forward_iter % backwards_freq == 0 and forward_iter > 0:
+
+            for backward_iter in range(maxit_b):
+
+                non_zeros = np.where(w[k] != 0)[0]
+                err_after_simplification = []
+                for j in non_zeros:
+
+                    if relearn_b:
+                        F_trial = F[k].difference({j})
+                        w_simple = np.zeros((d,1))
+                        w_simple[list(F_trial)] = np.linalg.lstsq(X[:, list(F_trial)], y, rcond=None)[0]
+                
+                    else:
+                        w_simple = np.copy(w[k])
+                        w_simple[j] = 0
+
+                    err_after_simplification.append(np.linalg.norm(X.dot(w_simple) - y))
+                j = np.argmin(err_after_simplification)
+                w_simple = np.copy(w[k])
+                w_simple[non_zeros[j]] = 0
+
+                # check for break condition on backward step
+                delta_p = err_after_simplification[j] - np.linalg.norm(X.dot(w[k]) - y)
+                if delta_p > 0.5*delta[k]: break
+
+                k = k-1;
+                F[k] = F[k+1].difference({j})
+                w[k] = np.zeros((d,1))
+                w[k][list(F[k])] = np.linalg.lstsq(X[:, list(F[k])], y,rcond=None)[0]
+
+    return w[k] 
